1e00351fbef3a1c32d30fed2cfb4e3f806dcae6c
e <2269041628@qq.com>
2022-01-06 17:51:01 +0800

    updated:2022-01-06

--- chapter12/06 文本查询程序/TextQuery.h
+++ chapter12/06 文本查询程序/TextQuery.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef TEXTQUERY_H
+#define TEXTQUERY_H
 
 #include <vector>
 #include <map>
@@ -7,61 +8,59 @@
 #include <fstream>
 #include <sstream>
 
+using std::vector; using std::string; using std::map; using std::set;
+using std::shared_ptr; using std::make_shared;
+using std::ifstream; using std::ostream; using std::istringstream;
+using std::endl;
 
-class QueryResult;
+class QueryResult; // 为了定义函数query的返回类型，这个定义是必需的
 
 class TextQuery {
 public:
-	using line_no = std::vector<std::string>::size_type;
-	TextQuery(std::ifstream&);
-	QueryResult query(const std::string&) const;
+	using line_no = vector<string>::size_type;
+	TextQuery(ifstream&);
+	QueryResult query(const string&) const;
 private:
-	std::shared_ptr<std::vector<std::string>> text;
-	std::map<std::string, std::shared_ptr<std::set<line_no>>> words_map;
+	shared_ptr<vector<string>> text; // 输入文件
+	map<string, shared_ptr<set<line_no>>> words_map; // 每个单词到它所在的行号的集合的映射
 };
 
 class QueryResult {
-	friend std::ostream& print(std::ostream&, const QueryResult&);
+	friend ostream& print(ostream&, const QueryResult&);
 public:
-	QueryResult(std::shared_ptr<std::vector<std::string>> p,
-		std::shared_ptr<std::set<TextQuery::line_no>> ln,
-		std::string w, unsigned n) :
-		text(p), lines(ln), sought(w), count(n) {}
+	QueryResult(shared_ptr<vector<string>> p, shared_ptr<set<TextQuery::line_no>> ln, string w, unsigned n) : text(p), lines(ln), sought(w), count(n) {}
 private:
-	std::shared_ptr<std::vector<std::string>> text;
-	std::shared_ptr<std::set<TextQuery::line_no>> lines;
-	std::string sought;
+	shared_ptr<vector<string>> text; // 输入文件
+	shared_ptr<set<TextQuery::line_no>> lines; // 出现的行号
+	string sought; // 查询单词
 	unsigned count = 0;
 };
 
 // constructors
-TextQuery::TextQuery(std::ifstream& infile) :
-	text(std::make_shared<std::vector<std::string>>())
-{
-	std::string line, word;
-	for (size_t lineNumber = 1; getline(infile, line); ++lineNumber) {
-		text->push_back(line);
-		std::istringstream iss(line);
-		while (iss >> word) {
-			auto& lines = words_map[word];
-			if (!lines)
-				lines = std::make_shared<std::set<line_no>>();
-			lines->insert(lineNumber);
+TextQuery::TextQuery(ifstream& infile) : text(make_shared<vector<string>>()) {
+	string line, word;
+	for (size_t lineNumber = 1; getline(infile, line); ++lineNumber) { // 对文件中每一行
+		text->push_back(line); // 保存此行文本
+		istringstream iss(line); // 将行文本分解成单词
+		while (iss >> word) { // 对行中每个单词
+			auto& lines = words_map[word]; // 如果单词不在words_map中，以之为下标在words_map中添加一项
+			if (!lines) // 当我们第一次遇到这个单词时，此指针为空
+				lines = make_shared<set<line_no>>(); // 分配一个新的set
+			lines->insert(lineNumber); // 将此行号插入set中
 		}
 	}
 }
 
 // members
-QueryResult TextQuery::query(const std::string& s) const
-{
-	static auto notFound = std::make_shared<std::set<line_no>>();
+QueryResult TextQuery::query(const string& s) const {
+	static auto notFound = make_shared<set<line_no>>();
 	auto pos = words_map.find(s);
 	if (pos == words_map.end())
 		return { text, notFound, s, 0 };
 	unsigned count = 0;
-	std::string word;
+	string word;
 	for (auto const& line : *text) {
-		std::istringstream iss(line);
+		istringstream iss(line);
 		while (iss >> word)
 			if (word == s)
 				++count;
@@ -70,11 +69,11 @@ QueryResult TextQuery::query(const std::string& s) const
 }
 
 // non-members
-std::ostream& print(std::ostream& os, const QueryResult& result)
-{
-	os << result.sought << " occurs " << result.count 
-	   << ((result.count > 1) ? " times" : " time") << std::endl;
+ostream& print(ostream& os, const QueryResult& result) {
+	os << result.sought << " occurs " << result.count << ((result.count > 1) ? " times" : " time") << endl;
 	for (auto const& n : *result.lines)
 		os << "\t(line " << n << ") " << result.text->at(n - 1) << '\n';
 	return os;
 }
+
+#endif
--- chapter12/06 文本查询程序/main.cpp
+++ chapter12/06 文本查询程序/main.cpp
@@ -1,37 +1,21 @@
-#include <fstream>
 #include <iostream>
 #include "TextQuery.h"
 
-void runQueries(std::ifstream& infile)
-{
+using std::cin; using std::cout;
+
+void runQueries(ifstream& infile) {
 	TextQuery tq(infile);
 	while (true) {
-		std::cout << "Enter word to look for, or q to quit: ";
-		std::string s;
-		if (!(std::cin >> s) || s == "q")
+		cout << "Enter word to look for, or q to quit: ";
+		string s;
+		if (!(cin >> s) || s == "q")
 			break;
-		print(std::cout, tq.query(s)) << std::endl;
+		print(cout, tq.query(s)) << endl;
 	}
 }
 
-//int main(int argc, char** argv)
-//{
-//	if (argc != 2) {
-//		std::cerr << "Usage: " << *argv << " infile\n";
-//		return -1;
-//	}
-//	std::ifstream file(*++argv);
-//	if (!file) {
-//		std::cerr << "could not open " << *argv << '\n';
-//		return -1;
-//	}
-//	runQueries(file);
-//	return 0;
-//}
-
-int main()
-{
-	std::ifstream file("storyDataFile.txt");
+int main() {
+	ifstream file("storyDataFile.txt");
 	runQueries(file);
 	return 0;
 }
